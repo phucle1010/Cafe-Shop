00ea69302e7e230c155a9033165a98df9555396c
Lê Phúc <lethephuc2002@gmail.com>
2022-11-08 21:39:27 +0700

    entityFramework_and_localStore

---
+++ QL_QuanCafe/QL_QuanCafe/Model/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_QuanCafe.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if ( _ins == null )
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+        public CafeShopEntities DB { get; set; }
+        private DataProvider()
+        {
+            DB = new CafeShopEntities();
+        }
+    }
+}
